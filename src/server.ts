354390d676bb41203edf733aa661f64c5065b045
gusmartins499 <gustavosm994@gmail.com>
2022-02-12 10:15:58 -0300

    create client

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import { routes } from "./routes";
+
 import packageJson from "../package.json";
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.get("/api", (request, response) => {
   return response.json({ message: `API version: ${packageJson.version}`})
